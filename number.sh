af63e22ae7bc36affb7c06bb226d8cdcc8d1ec37
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2023-10-25 10:20:49 -0300

    feat: adjust script

--- number.sh
+++ number.sh
@@ -1,1 +1,14 @@
-curl 'https://api.github.com/repos/codaqui/boletim-diario-seguranca/issues' | jq -r '.[0].number' >> numero_final.txt
+# Verify the last issue number
+curl 'https://api.github.com/repos/codaqui/boletim-diario-seguranca/issues' > response.json
+
+# How many issues are open?
+ISSUES_OPEN=$(cat response.json | jq '. | length')
+
+# Get the last issue number is not a Pull Request
+ISSUE_NUMBER=$(cat response.json | jq -r '.[] | select(.pull_request == null) | .number' | head -n 1)
+if [ -z "$ISSUE_NUMBER" ]
+then
+    echo "pr" > numero_final.txt
+else
+    echo $ISSUE_NUMBER > numero_final.txt
+fi
